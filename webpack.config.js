6ef049e28cee8bacf4736e19a1f4edf3200b711b
Lee Swee Ling <sweeling.lee@icarasia.com>
2021-11-16 10:29:28 +0800

    adding small chunks files for icons

--- webpack.config.js
+++ webpack.config.js
@@ -30,5 +30,181 @@ module.exports = {
                 },
             }
         ),
+        //action icons
+        new SVGSpritemapPlugin('src/action/*.svg',
+            {
+                // styles: 'dist/sprites_action.scss',
+                output: {
+                    filename: 'sprites_action.svg',
+                    svgo: false
+                },
+                sprite: {
+                    prefix: 'icon--',
+                    generate: {
+                        title: false
+                    }
+                },
+            }
+        ),
+        //dealerships icons
+        new SVGSpritemapPlugin('src/dealerships/*.svg',
+            {
+                // styles: 'dist/sprites_dealerships.scss',
+                output: {
+                    filename: 'sprites_dealerships.svg',
+                    svgo: false
+                },
+                sprite: {
+                    prefix: 'icon--',
+                    generate: {
+                        title: false
+                    }
+                },
+            }
+        ),
+        //editing icons
+        new SVGSpritemapPlugin('src/editing/*.svg',
+            {
+                // styles: 'dist/sprites_editing.scss',
+                output: {
+                    filename: 'sprites_editing.svg',
+                    svgo: false
+                },
+                sprite: {
+                    prefix: 'icon--',
+                    generate: {
+                        title: false
+                    }
+                },
+            }
+        ),
+        //icarsuite icons
+        new SVGSpritemapPlugin('src/icarsuite/*.svg',
+            {
+                // styles: 'dist/sprites_icarsuite.scss',
+                output: {
+                    filename: 'sprites_icarsuite.svg',
+                    svgo: false
+                },
+                sprite: {
+                    prefix: 'icon--',
+                    generate: {
+                        title: false
+                    }
+                },
+            }
+        ),
+        //listing icons
+        new SVGSpritemapPlugin('src/listing/*.svg',
+            {
+                // styles: 'dist/sprites_listing.scss',
+                output: {
+                    filename: 'sprites_listing.svg',
+                    svgo: false
+                },
+                sprite: {
+                    prefix: 'icon--',
+                    generate: {
+                        title: false
+                    }
+                },
+            }
+        ),
+        //media icons
+        new SVGSpritemapPlugin('src/media/*.svg',
+            {
+                // styles: 'dist/sprites_media.scss',
+                output: {
+                    filename: 'sprites_media.svg',
+                    svgo: false
+                },
+                sprite: {
+                    prefix: 'icon--',
+                    generate: {
+                        title: false
+                    }
+                },
+            }
+        ),
+        //navigation icons
+        new SVGSpritemapPlugin('src/navigation/*.svg',
+            {
+                // styles: 'dist/sprites_navigation.scss',
+                output: {
+                    filename: 'sprites_navigation.svg',
+                    svgo: false
+                },
+                sprite: {
+                    prefix: 'icon--',
+                    generate: {
+                        title: false
+                    }
+                },
+            }
+        ),
+        //object icons
+        new SVGSpritemapPlugin('src/object/*.svg',
+            {
+                // styles: 'dist/sprites_object.scss',
+                output: {
+                    filename: 'sprites_object.svg',
+                    svgo: false
+                },
+                sprite: {
+                    prefix: 'icon--',
+                    generate: {
+                        title: false
+                    }
+                },
+            }
+        ),
+        //products icons
+        new SVGSpritemapPlugin('src/products/*.svg',
+            {
+                // styles: 'dist/sprites_products.scss',
+                output: {
+                    filename: 'sprites_products.svg',
+                    svgo: false
+                },
+                sprite: {
+                    prefix: 'icon--',
+                    generate: {
+                        title: false
+                    }
+                },
+            }
+        ),
+        //response icons
+        new SVGSpritemapPlugin('src/response/*.svg',
+            {
+                // styles: 'dist/sprites_response.scss',
+                output: {
+                    filename: 'sprites_response.svg',
+                    svgo: false
+                },
+                sprite: {
+                    prefix: 'icon--',
+                    generate: {
+                        title: false
+                    }
+                },
+            }
+        ),
+        //social icons
+        new SVGSpritemapPlugin('src/social/*.svg',
+            {
+                // styles: 'dist/sprites_social.scss',
+                output: {
+                    filename: 'sprites_social.svg',
+                    svgo: false
+                },
+                sprite: {
+                    prefix: 'icon--',
+                    generate: {
+                        title: false
+                    }
+                },
+            }
+        ),
     ]
 };
